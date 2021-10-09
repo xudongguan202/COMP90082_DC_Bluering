a082fdd5b4e9175756ce7516fdc7587d53793bda
Steven <juxia@student.unimelb.edu.au>
2021-10-09 17:27:35 +1100

    [error fix] fuzzy search

--- searchChamberID.py
+++ searchChamberID.py
@@ -6,15 +6,15 @@ db = pymysql.connect(host='localhost',user='root',password='Bluering123.',databa
 
 cursor = db.cursor()
 
-job_number = 10
+job_number = 14
 chamber = 'PTW 30013 5122'
-clientname = 'ClientA_Name'
+clientname = 'ClientA_Nam'
 
 #sql = "SELECT chamber_ID from header WHERE job_number = %s" % job_number
 #sql = "SELECT chamber_ID from header WHERE chamber = %s" % chamber
 #sql = "SELECT chamber_ID from header WHERE job_number = %s AND chamber = '%s'" % (job_number,chamber)
 sql = "SELECT b.chamber_ID, b.chamber, a.job_number, a.clientname FROM client a RIGHT JOIN header b ON a.job_number = b.job_number WHERE LOWER(a.clientname) = '%s'"\
-      % (clientname.lower())
+      % ('%'+clientname.lower()+'%')
 
 cursor.execute(sql)
 rows = cursor.fetchall()
